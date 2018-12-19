513ecabfd15f3e9b51f7f23ba64d153679502402
Socrates Hlapolosa <socrates.hlapolosa@gmail.com>
2018-12-19 19:42:33 +0200

    added first test

--- src/main/java/com/sixt/platform/interview/service/ExecutionOrder.java
+++ src/main/java/com/sixt/platform/interview/service/ExecutionOrder.java
@@ -2,4 +2,7 @@ package com.sixt.platform.interview.service;
 
 public class ExecutionOrder {
 
+	public String run(VulnerabilityScript vulnerabilityScript) {
+		return null;
+	}
 }
--- src/test/java/com/sixt/platform/interview/service/ExecutionOrderTest.java
+++ src/test/java/com/sixt/platform/interview/service/ExecutionOrderTest.java
@@ -1,5 +1,17 @@
 package com.sixt.platform.interview.service;
 
-class ExecutionOrderTest {
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+
+public class ExecutionOrderTest {
+
+	@Test
+	public void should_return_null_on_empty_list() {
+		ExecutionOrder executionOrder = new ExecutionOrder();
+		Assert.assertNull(executionOrder.run(new VulnerabilityScript(1, new ArrayList<Integer>())));
+
+	}
 
 }
