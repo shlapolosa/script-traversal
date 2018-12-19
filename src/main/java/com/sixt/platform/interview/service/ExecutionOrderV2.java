63026e989c722cd4c7c9a52bbc55a13836bffaf5
Socrates Hlapolosa <socrates.hlapolosa@gmail.com>
2018-12-19 21:46:05 +0200

    added more comments

--- src/main/java/com/sixt/platform/interview/service/ExecutionOrderV2.java
+++ src/main/java/com/sixt/platform/interview/service/ExecutionOrderV2.java
@@ -3,6 +3,11 @@ package com.sixt.platform.interview.service;
 /**
  * in the second version, execute the last scripts first and for each scritp, execute its
  * dependencies first
+ *
+ * Note! does not explicitly check for cyclic dependencies, nor does it have a
+ * guard clause which would check for null or if a certain script had already been ran
+ *
+ *
  */
 public class ExecutionOrderV2 {
 
